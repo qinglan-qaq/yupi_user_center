1424e63b4e3db908f6b9a5574450bc4c348b5f66
qinglan-qaq <1710202731@qq.com>
2026-03-13 15:57:00 +0800

    完成ERRORCODE部分

--- src/main/java/com/lx/user_center/common/BaseResponse.java
+++ src/main/java/com/lx/user_center/common/BaseResponse.java
@@ -14,16 +14,26 @@ public class BaseResponse<T> implements Serializable {
     private int code;
     private T data;
     private String message;
+    private String description;
 
 //    构造函数
-    public BaseResponse(int code, T data, String message) {
+    public BaseResponse(int code, T data, String message,String description) {
         this.code = code;
         this.data = data;
         this.message = message;
+        this.description = description;
+    }
+
+    public BaseResponse(int code, T data,String message) {
+       this(code ,data,message,"");
     }
 
     public BaseResponse(int code, T data) {
-       this(code ,data,"");
+        this(code ,data,"","");
+    }
+
+    public BaseResponse() {
+        this(code ,data,message,"");
     }
 
 }
--- src/main/java/com/lx/user_center/common/ErrorCode.java
+++ src/main/java/com/lx/user_center/common/ErrorCode.java
@@ -1,4 +1,36 @@
 package com.lx.user_center.common;
 
-public class ErrorCode {
+/**
+ * 错误码
+ * @author lx
+ *
+ */
+public enum ErrorCode {
+
+    SUCCESS(0,"success",""),
+    PARAMS_ERROR(4000,"请求参数错误",""),
+    NULL_ERROR(4001,"请求参数为空",""),
+    NO_LOGIN(4002,"未登录",""),
+    NO_AUTH(4003,"没有权限","");
+
+    /**
+     * 状态码
+     */
+    private final  int code;
+
+    /**
+     * 状态码信息
+     */
+    private final String message;
+    
+    /**
+     * 状态码信息描述
+     */
+    private  final String description;
+
+    ErrorCode(int code, String message, String description) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+    }
 }
